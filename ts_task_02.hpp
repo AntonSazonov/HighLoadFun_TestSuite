438d60f2acce94f14659efa981529db3d49c4d92
SAN <anton.sazonov.dev@gmail.com>
2024-06-05 12:08:03 +0300

    Speed up task 2 data generation

--- ts_task_02.hpp
+++ ts_task_02.hpp
@@ -1,12 +1,37 @@
 #pragma once
 
+#define XXH_NO_STDLIB
+#define XXH_INLINE_ALL
+
+#if defined(__AVX512__)
+ #define XXH_VECTOR XXH_AVX512
+#elif defined(__AVX2__)
+ #define XXH_VECTOR XXH_AVX2
+#elif defined(__SSE2__)
+ #define XXH_VECTOR XXH_SSE2
+#else
+ #define XXH_VECTOR XXH_SCALAR
+#endif
+
+#include "xxhash.h"
+#include "bytell_hash_map.hpp"
+
+struct hasher {
+	uint32_t operator () ( std::string_view sv ) const {
+		return XXH32( sv.data(), sv.size(), 0 );
+	}
+};
+
+using set_t = ska::flat_hash_set <std::string_view, hasher>;
+//using set_t = std::unordered_set <std::string>;
+
 namespace ts {
 
 // Unique strings
 class task_02 : public task {
 	static constexpr size_t	t_divisor	= 1'000;	// score divisor
 	static constexpr size_t	t_lines		= 30'000'000;
-	static constexpr size_t	t_line_size	= 16; // max line size (with \n)
+	static constexpr size_t	t_line_size	= 16;		// max line size (with \n)
 	static constexpr size_t	t_in_size	= t_lines * t_line_size;
 	static constexpr size_t	t_out_size	= 7;
 
@@ -15,12 +40,12 @@ public:
 
 	bool generate_input( random_generator_t & generator ) override {
 
-	    std::unordered_set <std::string> tokens;
+		set_t tokens;
 
-		constexpr const char * chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#%*";
+		std::string_view char_lut{ "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#%*" };
 
 		int_distribution_t <> dist_len( 1, 15 );
-		int_distribution_t <> dist_chr( 0, strlen( chars ) - 1 );
+		int_distribution_t <> dist_chr( 0, char_lut.size() - 1 );
 
 		char * p_stdin = m_stdin.data<char>();
 
@@ -30,18 +55,16 @@ public:
 
 			char line[t_line_size];
 			for ( int i = 0; i < line_len; i++ ) {
-				line[i] = chars[dist_chr( generator )];
+				line[i] = char_lut[dist_chr( generator )];
 			}
 			line[line_len] = '\n';
 
-			tokens.insert( std::string( line, line_len ) );
-
 			std::memcpy( p_stdin, line, line_len + 1 );
+
+			tokens.insert( std::string_view( p_stdin, line_len ) );
+
 			p_stdin += line_len + 1;
 		}
-
-//		std::string expected = std::to_string( tokens.size() );
-//		strcpy( m_expected.data<char>(), expected.c_str() );
 
 		strcpy( m_expected.data<char>(), std::to_string( tokens.size() ).c_str() );
 
