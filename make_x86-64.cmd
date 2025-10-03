a08e561f501e37e28602b828f68bcf43488edcd1
AnSa <anton.sazonov.dev@gmail.com>
2025-10-03 14:33:34 +0300

    Update make_x86-64.cmd

--- make_x86-64.cmd
+++ make_x86-64.cmd
@@ -2,4 +2,4 @@
 cls
 echo.
 
-wsl -e g++ -Wall -std=c++17 -O3 -march=native -fno-exceptions -fno-rtti -fcf-protection=none ts_main.cpp -I. -I./3rd -s -o ts_main_x86-64
+wsl -e g++ -Wall -std=c++17 -O3 -march=native -fno-exceptions -fno-rtti -fcf-protection=none ts_main.cpp -I. -I./3rd -s -o hlf_ts_x86-64
